71dcc6e671ad3bbe918efbe75cc94e9226d5d83a
Anton Lintermans <anton.lintermans@student.kuleuven.be>
2023-12-11 12:30:49 +0100

    Update GPTLineGenerator.py

--- Experiments/GPT-2Constraints/GPTLineGenerator.py
+++ Experiments/GPT-2Constraints/GPTLineGenerator.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, set_seed, StoppingCriteriaList, MaxLengthCriteria, LogitsProcessorList, NoBadWordsLogitsProcessor, NoRepeatNGramLogitsProcessor
+from transformers import AutoTokenizer, AutoModelForCausalLM, set_seed, StoppingCriteriaList, MaxLengthCriteria, LogitsProcessorList, NoBadWordsLogitsProcessor, NoRepeatNGramLogitsProcessor, BitsAndBytesConfig
 from BeamSearchScorerConstrained import BeamSearchScorerConstrained
 from Constraint import ConstraintList, Constraint
 from SyllableConstraint import SyllableConstraint
@@ -6,15 +6,28 @@ from SongUtils import get_syllable_count_of_sentence
 import torch
 
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+use_4bit = True
+# Compute dtype for 4-bit base models
+bnb_4bit_compute_dtype = "float16"
+# Quantization type (fp4 or nf4)
+bnb_4bit_quant_type = "nf4"
+use_nested_quant = False
+
+compute_dtype = getattr(torch, "float16")
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=use_4bit,
+    bnb_4bit_quant_type=bnb_4bit_quant_type,
+    bnb_4bit_compute_dtype=compute_dtype,
+    bnb_4bit_use_double_quant=use_nested_quant,
+)
+
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", token= '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',)
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf",
-                                                torch_dtype=torch.bfloat16, 
                                                 low_cpu_mem_usage=True, 
                                                 token= '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',
-                                                # max_memory={"cpu": "11GIB"},
-                                                # offload_state_dict=True,
-                                                # offload_folder = '/Volumes/Samsung\ SSD/offload'
-                                                load_in_8bits=True
+                                                quantization_config=bnb_config,
                                                 )    
 
 set_seed(42)
