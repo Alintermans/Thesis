620f7e2027af567cfca864d1701497053ae8fe2e
Anton Lintermans <anton.lintermans@student.kuleuven.be>
2024-04-07 17:11:25 +0200

    Update Chat models correct endocing schema

--- Experiments/ConstrainedParodieGenerator/LanguageModels/Mistral8x7BItV01.py
+++ Experiments/ConstrainedParodieGenerator/LanguageModels/Mistral8x7BItV01.py
@@ -20,12 +20,12 @@ class Mistral8x7BItV01(LM):
     
     def prepare_prompt(self, system_prompt, context_prompt, assistant_prompt):
         messages = [
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": context_prompt},
-            {"role": "assistant", "content": assistant_prompt}
+            {"role": "user", "content": system_prompt + context_prompt}
         ]
 
-        tokenized_prompt =  self.tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
+        tokenized_prompt =  self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True, return_tensors="pt")
+        tokenized_prompt += '\n' + assistant_prompt
+        tokenized_prompt = self.tokenizer.encode(tokenized_prompt, return_tensors="pt") 
         prompt = self.tokenizer.decode(tokenized_prompt[0], skip_special_tokens=True)
 
         return prompt, tokenized_prompt
